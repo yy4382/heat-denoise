2373fb5bdc71ab26d1e9a0ecf79e54747c7ecbcd
Chris Yang <yy4382@outlook.com>
2023-12-21 23:46:40 +0800

    2

--- main.py
+++ main.py
@@ -13,57 +13,73 @@ def get_images() -> list:
 def image2matrix(image: str) -> np.ndarray:
     img = cv2.imread(image)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = cv2.resize(img, (32, 32))
+    img = cv2.resize(img, (256, 256))
     return img
 
 
-def denoise(image: np.ndarray, *, t: float, a: float) -> np.ndarray:
-    MAX = 5
-    height = image.shape[1]
-    width = image.shape[0]
+class denoise_img:
+    def __init__(self) -> None:
+        self.MAX = 10
+        self.a = 1e-6
+        self.t = 1e-6
 
-    def cal_phi(nx, ny) -> float:
-        result = 0.0
-        for i in range(width):
-            for j in range(height):
-                result += (
-                    image[j][i]
-                    * math.cos(math.pi * ny * i / width)
-                    * math.cos(math.pi * nx * j / height)
-                )
-        result = result * 4 / width / height
-        return result
+    def cal_phi(self, image: np.ndarray) -> np.ndarray:
+        def cal_phi_dot(nx, ny) -> float:
+            result = 0.0
+            for i in range(self.width):
+                for j in range(self.height):
+                    result += (
+                        image[j][i]
+                        * math.cos(math.pi * ny * i / self.width)
+                        * math.cos(math.pi * nx * j / self.height)
+                    )
+            result = result * 4 / self.width / self.height
+            return result
 
-    def cal_miu(nx, ny) -> float:
-        return (math.pi * nx / height) ** 2 + (math.pi * ny / width) ** 2
+        phi = np.zeros((self.MAX, self.MAX), dtype=np.float32)
+        for nx in range(1, self.MAX):
+            for ny in range(1, self.MAX):
+                phi[nx][ny] = cal_phi_dot(nx, ny)
+        print(phi)
+        return phi
 
-    def cal_dot(x, y) -> int:
-        result = 0.0
-        for nx in range(1, MAX):
-            for ny in range(1, MAX):
-                print(cal_phi(nx, ny))
-                result += (
-                    cal_phi(nx, ny)
-                    * math.exp(-a * cal_miu(nx, ny) * t)
-                    * math.cos(math.pi * nx * x / height)
-                    * math.cos(math.pi * ny * y / width)
-                )
-        return max(0, int(result))
+    def denoise(self, image: np.ndarray) -> np.ndarray:
+        self.height = image.shape[1]
+        self.width = image.shape[0]
+        phi = self.cal_phi(image)
+        def cal_miu(nx, ny) -> float:
+            return (math.pi * nx / self.height) ** 2 + (math.pi * ny / self.width) ** 2
 
-    denoised_image = np.zeros((width, height), dtype=np.uint8)
-    for i in range(width):
-        for j in range(height):
-            dot = cal_dot(i, j)
-            # assert dot < 0
-            denoised_image[i][j] = dot
-            # print(i, j, dot)
-    return denoised_image
+        def cal_dot(x, y) -> int:
+            result = 0.0
+            for nx in range(1, self.MAX):
+                for ny in range(1, self.MAX):
+                    # print(cal_phi(nx, ny))
+                    result += (
+                        phi[nx][ny]
+                        * math.exp(-self.a * cal_miu(nx, ny) * self.t)
+                        * math.cos(math.pi * nx * x / self.height)
+                        * math.cos(math.pi * ny * y / self.width)
+                    )
+            return max(0, int(result))
+
+
+        denoised_image = np.zeros((self.width, self.height), dtype=np.uint8)
+        for i in range(self.height):
+            for j in range(self.width):
+                dot = cal_dot(i, j)
+                # assert dot < 0
+                denoised_image[i][j] = dot
+                # print(i, j, dot)
+        return denoised_image
 
 
 if __name__ == "__main__":
     images = get_images()
     # cv2.imshow("original_image", image2matrix(images[0]))
     # cv2.waitKey(0)
-    denoised_image = denoise(image2matrix(images[0]), a=0.1, t=0.1)
+    den = denoise_img()
+    denoised_image = den.denoise(image2matrix(images[0]))
     cv2.imshow("denoised_image", denoised_image)
     cv2.waitKey(0)
+    # cv2.imwrite("/Users/chris/projects.localized/heat-denoise/data/de/denoised.jpeg", denoised_image)
